4632af1ed8419529da6e4ca0f6f3bf5b91186eb7
pahakrai <rai.pahak@gmail.com>
2022-01-06 16:34:06 +0545

    section updates

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import NextLink from 'next/link'
 import {
+  Button,
   Container,
   Box,
   Heading,
@@ -6,6 +8,10 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import styles from '../styles/Home.module.css'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 
 const Home = () => {
   return (
@@ -44,11 +50,42 @@ const Home = () => {
             maxWidth="100px"
             display="inline-block"
             borderRadius="full"
-            src="/images/takya.jpg"
+            src="/images/pahak.png"
             alt="Profile Image"
           />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          I am a full stack developer looking for more freelance opportunities
+          to provide solutions as services. If I am not coding I would be
+          dabbling with music or hiking.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1987</BioYear>
+          Born in Kathmandu, Nepal
+        </BioSection>
+        <BioSection>
+          <BioYear>2012</BioYear>
+          Completed the Master&apos;s Mobile Computing at University of West
+          London
+        </BioSection>
+      </Section>
     </Container>
   )
 }
