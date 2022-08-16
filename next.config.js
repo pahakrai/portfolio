8a239219feaa76d6aee8960001fa1df90a5f8192
pahakrai <tulakra@gmail.com>
2022-08-16 19:18:26 +0800

    invoice removed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const path = require('path')
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   },
